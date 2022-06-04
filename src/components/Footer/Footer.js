90132aed429c7c4605765c80a335d2f8ea4c8b96
Manglam Paliwal <paliwalmanglam16@gmail.com>
2022-06-04 16:29:20 +0530

    Add existing project files to Git

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/manglam16'
+      className='link footer__link'
+    >
+      Created with ❤️ By Manglam Paliwal
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,59 @@
+const header = {
+    homepage: 'https://manglam16.github.io',
+    title: 'MP',
+  }
+
+  const about = {
+    name: 'Manglam Paliwal',
+    role: 'Software Developer',
+    description:
+      'Forward looking software engineer with background in competitive programming, creating and executing innovative solutions using web and android development over various projects. Skilled at time management in a development over various projects. Skilled at time management in a fast-pace deadline driven environment.',
+    resume: 'https://drive.google.com/file/d/1KL4Kbozal0eayvru6r44MMqH48eg4CXB/view?usp=sharing',
+    social: {
+      linkedin: 'https://linkedin.com',
+      github: 'https://github.com/manglam16',
+    },
+  }
+  
+  const projects = [
+    {
+      name: 'OffBeat',
+      description:
+        '',
+      stack: ['Kotlin', 'Firebase', 'Android Studio'],
+      sourceCode: 'https://github.com/manglam16/offbeat-1',
+    },
+    {
+      name: 'DesiDukaan',
+      description:
+        'Still in development phase',
+      stack: ['', '', ''],
+      sourceCode: '',
+      livePreview: '',
+    }
+  ]
+  
+  const skills = [
+    'HTML',
+    'CSS',
+    'C++',
+    'C',
+    'Java',
+    'JavaScript',
+    'React.js',
+    'Redux',
+    'Git',
+    'Data Structure',
+    'Algorithms',
+    'Web Development',
+    'Android Development'
+
+
+  ]
+  
+  const contact = {
+    email: 'paliwalmanglam16@gmail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
+  
